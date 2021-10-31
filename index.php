2e4d0363309b5140238fde764cd394a847387e93
TrungKien07102001 <90234857+TrungKien07102001@users.noreply.github.com>
2021-10-31 12:49:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BTL</title>
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+    <div>
+        <div class="container">
+        <header>
+            <a href="#"><img src="img/Logo.png" alt=""></a>
+
+        </header>
+        <nav>
+            <div class="menu">
+            
+            <ui>
+                <li><a href="#">Đăng Ký</a></li>
+                <li><a href="#">Đăng Nhập</a></li>
+            </ui>
+
+            </div>
+
+        </nav>
+        <div id="anh4"><a href="#"><img src="img/anh4.png" alt=""></a></div>
+        
+
+        <div class="Email">
+            <h1>Trello giúp các nhóm đẩy nhanh công việc.</h1>
+            <p>Cộng tác, quản lý dự án và đạt đến đỉnh cao năng suất mới.
+               Từ tòa nhà cao tầng đến văn phòng tại nhà,
+               cách thức làm việc của nhóm bạn là duy nhất.
+               Hãy hoàn thành tất cả với Trello.</p>
+        </div>
+        <form action="/signup"><input type="email" name="email" aria-label="Email" placeholder="Email." data-testid="ui-email-signup-input"></form>
+        <div class="DKMP">
+            <button>Đăng ký - hoàn toàn miễn phí</button>
+        </div>
+        <div id="anh2"><a href="#"><img src="img/anh2.png" alt=""></a></div>
+
+        </div>
+        
+    </div>
+    
+   
+    <!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/tiktok.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/youtube.png"/></a>
+                </li>
+
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">Nguyễn Trung Kiên</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
+</body>
+</html>
